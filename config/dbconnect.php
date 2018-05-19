6a3955574f2eb9882351376e8fb622fffa44e7cb
Arifullah Jan <34775308+ajanbscs16seecs@users.noreply.github.com>
2018-05-19 13:37:50 +0500

    shell

---
+++ config/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+$databaseHost = '182.50.133.91:3306';
+$databaseName = 'practicereal';
+$databaseUsername = 'pr_dbuser';
+$databasePassword = 'eI1ys!22';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+
+?>
